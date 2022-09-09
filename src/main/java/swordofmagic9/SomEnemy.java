36d2058ad86995bf56d78b1512f5f29014a897e2
tyem0802 <108391699+tyem0802@users.noreply.github.com>
2022-09-09 20:34:20 +0900

    a

--- src/main/java/swordofmagic9/SomEnemy.java
+++ src/main/java/swordofmagic9/SomEnemy.java
@@ -2,6 +2,4 @@ package swordofmagic9;
 
 public interface SomEnemy extends SomEntity{
 
-
-
 }
---
+++ src/main/java/swordofmagic9/SomItemTable.java
@@ -0,0 +1,17 @@
+package swordofmagic9;
+
+public interface SomItemTable {
+
+    SomItem getItem();
+    void setItem();
+
+    double getPercent();
+    void setPercent();
+
+    int getMinAmount();
+    void setMinAmount();
+
+    int getMaxAmount();
+    void setMaxAmount();
+
+}
